bd75bc80eb6c225892a17b6f3df840c2d2639bac
lmaxim <maxim.sergeevich@gmail.com>
2013-08-06 19:36:40 +0400

    grep modification

--- sms24x7rusender.sh
+++ sms24x7rusender.sh
@@ -56,7 +56,7 @@ fi
 
 touch $lockfile
 
-sendresult=`curl -X POST --silent -d "method=push_msg&format=json&api_v=1.1&email=$login&password=$password&text=$message&phones=$phones" https://api.sms24x7.ru/ 2>&1 | grep -Eo '(?<="err_code":)(\d)+'`
+sendresult=`curl -X POST --silent -d "method=push_msg&format=json&api_v=1.1&email=$login&password=$password&text=$message&phones=$phones" https://api.sms24x7.ru/ 2>&1 | grep -Po '(?<="err_code":)(\d)+'`
 
 if [ $sendresult -ne 0 ]; then
 	#https://outbox.sms24x7.ru/api_manual/errors.html
