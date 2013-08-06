45e3fd8276deec8af49c5981c809cdf2f685a1d1
lmaxim <maxim.sergeevich@gmail.com>
2013-08-06 19:30:48 +0400

    added script

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-sms24sender.sh
+sms24x7rusender.sh
 ==============
 
-small shell script that allows to send sms thru http://sms24x7.ru/api/ service
+<h3>Description</h3>
+A small shell script that allows to send sms thru http://sms24x7.ru/api/ service
+
+MAC OS X partially supported
+
+<h3>How it works</h3>
+First call determine and provide a few commands that needs to be executed under necessary privileges
+```
+sudo touch /var/log/smsnotifier.log
+sudo chmod 0776 /var/log/smsnotifier.log
+```
+And check lock-file permissions
+```
+sudo mkdir -p /var/lock/smsnotifier/
+sudo chmod -R 0776 /var/lock/smsnotifier/
+```
+After successful setup, the script will try to send messages thru sms24x7.ru service, in case of unsuccessful response it will be writen in log file
+
+<h3>Dependencies</h3>
+- curl 7.27.0+
+- grep 2.12+
+
+<h3>Example</h3>
+
+Here's an example:
+```
+$ ./smssend.sh "message" "login" "password" "[phone]"
+$ ./smssend.sh "message" "login" "password" "[phone1,phone2,phoneN]"
+```
+
+<h3>Setup</h3>
+in file sms24x7rusender.sh:3
+'theta' is a value in seconds that determine delay between two similar requests
---
+++ sms24x7rusender.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+theta=1800 # 30 min in seconds
+
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 <message> <login> <password> [<phone>]" >&2
+  exit 1
+fi
+
+logfile="/var/log/smsnotifier.log"
+
+if [ ! -f $logfile ]; then
+
+echo "Create logfile:\n\
+sudo touch $logfile\n\
+sudo chmod 0776 $logfile";
+
+exit 1
+
+fi
+
+lockdir="/var/lock/smsnotifier/"
+
+if [ ! -d $lockdir ]; then
+
+echo "Create lockfile:\n\
+sudo mkdir -p $lockdir\n\
+sudo chmod -R 0776 $lockdir";
+
+exit 1;
+	
+fi
+
+message=$1
+login=$2
+password=$3
+phones=$4
+
+if which md5sum >/dev/null; then
+	lockfile=$lockdir`echo -n $1 | md5sum | awk '{print $1}'``echo ".lock"`
+else 
+	lockfile=$lockdir`echo -n $1 | md5 | awk '{print $1}'``echo ".lock"`
+fi
+
+if [ -f $lockfile ]; then
+	filemtime=`stat -c %Y $lockfile 2>/dev/null`;
+	if [ -z $filemtime ]; then
+		filemtime=`stat -f %m $lockfile 2>/dev/null`;
+	fi
+	currtime=`date +%s`;
+	diff=$(($currtime - $filemtime));
+	if [ $diff -lt $theta ]; then
+		exit 0
+	fi
+fi
+
+touch $lockfile
+
+sendresult=`curl -X POST --silent -d "method=push_msg&format=json&api_v=1.1&email=$login&password=$password&text=$message&phones=$phones" https://api.sms24x7.ru/ 2>&1 | grep -Eo '(?<="err_code":)(\d)+'`
+
+if [ $sendresult -ne 0 ]; then
+	#https://outbox.sms24x7.ru/api_manual/errors.html
+	echo "error at sending message code: $sendresult" >> $logfile
+	rm -f $lockfile
+	exit 1
+fi
+
+exit 0
